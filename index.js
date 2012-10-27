c4a866dd6a441b6f6062cc08015cd83ae9f27a3c
TJ Holowaychuk <tj@vision-media.ca>
2012-10-27 14:21:52 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,152 @@
+
+/**
+ * Module dependencies.
+ */
+
+var request = require('superagent')
+  , exec = require('child_process').exec
+  , debug = require('debug')('component-bot')
+  , command = require('shelly')
+  , noop = function(){};
+
+/**
+ * Expose `Bot`.
+ */
+
+module.exports = Bot;
+
+/**
+ * Initialize a new Bot with `user` / `pass`.
+ *
+ * @param {String} user
+ * @param {String} pass
+ * @api public
+ */
+
+function Bot(user, pass) {
+  this.user = user;
+  this.pass = pass;
+  this.clonePath = 'clones';
+}
+
+/**
+ * Create an issue with the given `title`, `body`, and `options`.
+ *
+ *     bot
+ *     .issue('visionmedia/superagent', 'Some issue', 'Check this out')
+ *     .end(function(res){
+ *       console.log(res.status);
+ *       console.log(res.body);
+ *     })
+ *
+ * @param {String} repo
+ * @param {String} title
+ * @param {String} body
+ * @param {Object} [options]
+ * @return {Request}
+ * @api public
+ */
+
+Bot.prototype.issue = function(repo, title, body, options){
+  debug('issue %s %s', repo, title);
+  options = options || {};
+  options.title = title;
+  options.body = body;
+  return this.post('/repos/' + repo + '/issues')
+    .send(options)
+};
+
+/**
+ * Commit to `repo` with the given `msg` and invoke `fn(err)`.
+ *
+ *    bot.commit('visionmedia/superagent', 'add license property to component.json');
+ *
+ * @param {String} repo
+ * @param {String} msg
+ * @param {Function} [fn]
+ * @api public
+ */
+
+Bot.prototype.commit = function(repo, msg, fn){
+  debug('commit %s "%s"', repo, msg);
+  var author = this.user + ' <forms@vision-media.ca>';
+  var cmds = [];
+  cmds.push('cd ?/?');
+  cmds.push('git commit -a -m "?" --author="?"');
+  cmds.push('git push origin master');
+  var cmd = command(cmds.join('&&'), this.clonePath, repo, msg, author);
+  debug('exec `%s`', cmd);
+  exec(cmd, fn || noop);
+};
+
+/**
+ * Clone `repo` to the `/tmp` directory and invoke `fn(err, path)`.
+ *
+ *    bot.clone('visionmedia/superagent', fn);
+ *
+ * @param {String} repo
+ * @param {Function} fn
+ * @api public
+ */
+
+Bot.prototype.clone = function(repo, fn){
+  debug('clone %s', repo);
+  var path = this.clonePath + '/' + repo;
+  var auth = this.user + ':' + this.pass;
+  var url = 'https://' + auth + '@github.com/' + repo + '.git';
+  var cmd = command('rm -fr ? && git clone ? --depth 1 ?', path, url, path);
+
+  debug('exec `%s`', cmd);
+  exec(cmd, function(err){
+    if (err) return fn(err);
+    fn(null, path);
+  });
+};
+
+/**
+ * Fork `repo`.
+ *
+ * @param {String} repo
+ * @return {Request}
+ * @api public
+ */
+
+Bot.prototype.fork = function(repo){
+  debug('fork %s', repo);
+  return this.post('/repos/' + repo + '/forks')
+    .send({});
+};
+
+/**
+ * Send pull-request to `repo` with the given `title` and `body`.
+ *
+ * @param {String} repo
+ * @param {String} title
+ * @param {String} body
+ * @return {Request}
+ * @api public
+ */
+
+Bot.prototype.pullrequest = function(repo, title, body, options){
+  options = options || {};
+  options.title = title;
+  options.body = body;
+  options.base = 'master';
+  options.head = this.user + ':master';
+  return this.post('/repos/' + repo + '/pulls')
+    .send(options);
+};
+
+/**
+ * POST to `path`.
+ *
+ * @param {String} path
+ * @return {Request}
+ * @api private
+ */
+
+Bot.prototype.post = function(path){
+  return request
+    .post('https://api.github.com' + path)
+    .auth(this.user, this.pass)
+};
